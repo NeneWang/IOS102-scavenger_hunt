41505629cd7d76cab274f7b107855995e1a68c2e
NelsonWang <wangnelson2@gmail.com>
2024-03-02 00:22:09 -0500

    Initial Commit

---
+++ scavenger_hunt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scavenger_hunt
+//
+//  Created by Nene Wang  on 3/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ scavenger_huntUITests/scavenger_huntUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  scavenger_huntUITestsLaunchTests.swift
+//  scavenger_huntUITests
+//
+//  Created by Nene Wang  on 3/2/24.
+//
+
+import XCTest
+
+final class scavenger_huntUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
